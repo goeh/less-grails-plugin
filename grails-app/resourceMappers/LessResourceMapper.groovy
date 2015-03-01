c2ec55a9a2ae352fdddb2c4d658fba1135192fc8
Goran Ehrsson <goran@technipelago.se>
2015-03-01 13:00:26 +0100

    You can now configure default import paths with 'grails.resources.less.default.importPath'

--- grails-app/resourceMappers/LessResourceMapper.groovy
+++ grails-app/resourceMappers/LessResourceMapper.groovy
@@ -1,4 +1,3 @@
-import org.codehaus.groovy.grails.commons.GrailsApplication
 import org.grails.plugin.resource.mapper.MapperPhase
 
 /**
@@ -11,7 +10,7 @@ class LessResourceMapper {
     static phase = MapperPhase.GENERATION
     static defaultIncludes = [ '**/*.less' ]
 
-    GrailsApplication grailsApplication
+    def grailsApplication
     def lessCompilerService
 
     def paths = [].asSynchronized()
@@ -20,17 +19,22 @@ class LessResourceMapper {
         File lessFile = resource.processedFile
         File cssFile = new File(lessFile.absolutePath + '.css')
 
-        def importPath = grailsApplication.parentContext.getResource(resource.originalUrl)?.file?.parentFile?.absolutePath
+        if(paths.isEmpty()) {
+            addDefaultPath(grailsApplication.config.grails.resources.less.default.importPath ?: ['less'])
+        }
+        def importPath = grailsApplication.mainContext.getResource(resource.originalUrl)?.file?.parentFile?.absolutePath
         if (importPath) {
-            def order = resource.tagAttributes.order ?: 10
-            log.debug "Adding import path [${importPath}][order: ${order}] for resource [${resource}]"
-            paths << [path:importPath, order:order]
-            paths.sort {it.order}
+            if(! paths.find{it.path == importPath}) {
+                def order = resource.tagAttributes.order ?: 10
+                log.debug "Adding import path [${importPath}][order: ${order}] for resource [${resource}]"
+                paths << [path:importPath, order:order]
+                paths.sort {it.order}
+            }
         }
 
         try {
             log.debug "Compiling LESS file [${lessFile}] into [${cssFile}]"
-            lessCompilerService.compile (lessFile, cssFile, paths.collect {it.path})
+            lessCompilerService.compile (lessFile, cssFile, paths*.path)
             resource.processedFile = cssFile
             resource.contentType = 'text/css'
             resource.tagAttributes.rel = 'stylesheet'
@@ -40,4 +44,24 @@ class LessResourceMapper {
         }
     }
 
+    private void addDefaultPath(defaultPath) {
+        def applicationContext = grailsApplication.mainContext
+
+        if(!(defaultPath instanceof Collection)) {
+            defaultPath = [defaultPath]
+        }
+        int order = 1
+        for(path in defaultPath) {
+            def importPath = applicationContext.getResource(path)?.file?.absolutePath
+            if (importPath) {
+                log.debug "Adding default import path [${importPath}][order: ${order}]"
+                if(! paths.find{it.path == importPath}) {
+                    paths << [path: importPath, order: order++]
+                }
+            } else {
+                log.error "Default import path not found: $path"
+            }
+        }
+        paths.sort {it.order}
+    }
 }
